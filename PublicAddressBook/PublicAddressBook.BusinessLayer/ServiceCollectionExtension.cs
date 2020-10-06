05ec10f04916f3646ae4480e549cee57f281a684
zagarzg <zagar.zg@gmail.com>
2020-10-06 18:59:40 +0200

    Add ContactService and register it

---
+++ PublicAddressBook/PublicAddressBook.BusinessLayer/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PublicAddressBook.BusinessLayer.Implementations;
+using PublicAddressBook.BusinessLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicAddressBook.BusinessLayer
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterBusinessServices(this IServiceCollection services)
+        {
+            services.AddTransient<IContactService, ContactService>();
+        }
+    }
+}
