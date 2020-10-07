eb9b4bc168308575a8feda3ac8877285e437569f
zagarzg <zagar.zg@gmail.com>
2020-10-07 19:47:35 +0200

    Setup repository pattern

---
+++ PublicAddressBook/PublicAddressBook.BusinessLayer/Mappings.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using PublicAddressBook.DomainLayer.Entities;
+using PublicAddressBook.PersistanceLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PublicAddressBook.BusinessLayer
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+
+            CreateMap<Contact, ContactDTO>()
+                .ForMember(dest =>
+                dest.FullName,
+                opt => opt.MapFrom(src => src.FirstName + " " + src.LastName))
+                .ReverseMap()
+                .ForMember(dest =>
+                dest.FirstName,
+                opt => opt.MapFrom(src => src.FullName.Split(new[] { ' ' }, 2).ToList()[0]))
+                .ForMember(dest =>
+                dest.LastName,
+                opt => opt.MapFrom(src => src.FullName.Split(new[] { ' ' }, 2).ToList()[1]));
+
+            CreateMap<PhoneNumber, PhoneNumberDTO>().ReverseMap();
+
+        }
+
+        
+    }
+}
---
+++ PublicAddressBook/PublicAddressBook.PersistanceLayer/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PublicAddressBook.DomainLayer;
+using PublicAddressBook.PersistanceLayer.Interfaces;
+using PublicAddressBook.PersistanceLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicAddressBook.PersistanceLayer
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigurePersistanceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add database
+            services.AddDbContext<ApplicationDbContext>(o =>
+            {
+                o.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+                o.EnableSensitiveDataLogging();
+                o.EnableDetailedErrors();
+            });
+
+            // Configure context for DI
+            services.AddTransient<ApplicationDbContext>();
+
+            // add repositories to DI
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            //services.AddTransient<IPokemonRepository, PokemonRepository>();
+        }
+    }
+    
+}
