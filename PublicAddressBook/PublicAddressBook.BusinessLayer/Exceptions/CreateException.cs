f3ed64983b715100a0a7acb562f22bba6f4a5467
zagarzg <zagar.zg@gmail.com>
2020-10-08 00:04:00 +0200

    Add exceptions and global exception handling

---
+++ PublicAddressBook/PublicAddressBook.BusinessLayer/Exceptions/CreateException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PublicAddressBook.BusinessLayer.Exceptions
+{
+    public class CreateException : Exception
+    {
+        public CreateException(string message, Exception ex = null)
+            : base($"Could not create entity.\nMessage: {message}", ex)
+        {
+        }
+
+        public CreateException(Exception ex = null)
+            : base($"Could not create entity.", ex)
+        {
+        }
+    }
+}
---
+++ PublicAddressBook/PublicAddressBook.BusinessLayer/Exceptions/UpdateException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PublicAddressBook.BusinessLayer.Exceptions
+{
+    public class UpdateException: Exception
+    {
+        public UpdateException(Guid id, string message, Exception ex = null)
+            : base($"Could not update entity.\nEntity id: {id}\nMessage: {message}", ex)
+        {
+        }
+
+        public UpdateException(Guid id, Exception ex = null)
+            : base($"Could not update entity.\nEntity id: {id}", ex)
+        {
+        }
+    }
+}
